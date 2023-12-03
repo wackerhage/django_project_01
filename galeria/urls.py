f3b4f939f5b32b8d5fb58aeb02b2453cfff70042
Dominique Wackerhage <110652367+wackerhage@users.noreply.github.com>
2023-12-02 21:58:49 -0300

    Add files via upload

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from galeria.views import index, index2
+from galeria.views import index, index2, index3
 
 urlpatterns = [
     path('', index),
     path('index2/', index2, name='index2'),
+    path('index2/index3', index3, name='index3'),
 ]
